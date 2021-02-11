8515ef76a35c4a5771ca69967eb69d40dc0ac016
GreeZAld <>
2021-02-11 11:19:55 +0300

    server port updated

--- src/server.js
+++ src/server.js
@@ -2,9 +2,10 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('./src/notes.json')
 const middlewares = jsonServer.defaults()
+const port = process.env.PORT || 4000;
 
 server.use(middlewares)
 server.use(router)
-server.listen(9000, () => {
+server.listen(port, () => {
   console.log('JSON Server is running')
 })
